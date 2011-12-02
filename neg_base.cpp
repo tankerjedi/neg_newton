4698ae1376b3cc215cea3b797c52900650ff15c5
tankerjedi <tankerjedi@gmail.com>
2011-12-02 16:18:13 +0100

    solve equation system function, there are problems now

--- neg_base.cpp
+++ neg_base.cpp
@@ -25,7 +25,9 @@ if (argc > 6) {mu = atof(argv[6]);}
 if (argc > 7) {iteration_limit = atoi(argv[7]);}
 
 
-solve_equation(true);
+//solve_equation(true);
+
+solve_equation_system();
 
 return 0;
 }
@@ -37,42 +39,42 @@ return 0;
 
 double income_region1(double wage1)
 {
-return mu * lambda * wage1 + (1 - mu) / 2;
+	return mu * lambda * wage1 + (1 - mu) / 2;
 };
 
 double income_region2(double wage2)
 {
-return mu * (1 - lambda) * wage2 + (1 - mu) / 2;
+	return mu * (1 - lambda) * wage2 + (1 - mu) / 2;
 };
 
 double price_index_region1(double wage1, double wage2)
 {
-return pow(lambda * pow(wage1, 1 - sigma) + (1 - lambda) * pow(wage2 * T, 1 - sigma),(1 / (1 - sigma)));
+	return pow(lambda * pow(wage1, 1 - sigma) + (1 - lambda) * pow(wage2 * T, 1 - sigma),(1 / (1 - sigma)));
 };
 
 double price_index_region2(double wage1, double wage2)
 {
-return pow(lambda * pow(wage1 * T, 1 - sigma) + (1 - lambda) * pow(wage2, 1 - sigma),(1 / (1 - sigma)));
+	return pow(lambda * pow(wage1 * T, 1 - sigma) + (1 - lambda) * pow(wage2, 1 - sigma),(1 / (1 - sigma)));
 };
 
 double wage_region1(double income1, double income2, double price_index1, double price_index2)
 {
-return pow(income1 * pow(price_index1, sigma - 1) + income2 * pow(price_index2 / T, sigma -1),(1 /  sigma));
+	return pow(income1 * pow(price_index1, sigma - 1) + income2 * pow(price_index2 / T, sigma -1),(1 /  sigma));
 };
 
 double wage_region2(double income1, double income2, double price_index1, double price_index2)
 { 
-return pow(income1 * pow(price_index1 / T, sigma - 1) + income2 * pow(price_index2 , sigma -1),(1 /  sigma));
+	return pow(income1 * pow(price_index1 / T, sigma - 1) + income2 * pow(price_index2 , sigma -1),(1 /  sigma));
 };
 
 double real_wage_region1(double wage1, double price_index1)
 {
-return wage1 * pow(price_index1, -mu);
+	return wage1 * pow(price_index1, -mu);
 };
 
 double real_wage_region2(double wage2, double price_index2)
 {
-return wage2 * pow(price_index2, -mu);
+	return wage2 * pow(price_index2, -mu);
 };
 
 //simplified equations
@@ -80,24 +82,24 @@ return wage2 * pow(price_index2, -mu);
 
 double new_wage_region1(double wage1, double wage2)
 {
-return pow(income_region1(wage1) * pow(price_index_region1(wage1,wage2), sigma - 1) + income_region2(wage2) * pow(price_index_region2(wage1,wage2) / T, sigma -1),(1 /  sigma)) - wage1;
+	return pow(income_region1(wage1) * pow(price_index_region1(wage1,wage2), sigma - 1) + income_region2(wage2) * pow(price_index_region2(wage1,wage2) / T, sigma -1),(1 /  sigma)) - wage1;
 };
 
 double new_wage_region2(double wage1, double wage2)
 { 
-return pow(income_region1(wage1) * pow(price_index_region1(wage1,wage2) / T, sigma - 1) + income_region2(wage2) * pow(price_index_region2(wage1,wage2) , sigma -1),(1 /  sigma)) - wage2;
+	return pow(income_region1(wage1) * pow(price_index_region1(wage1,wage2) / T, sigma - 1) + income_region2(wage2) * pow(price_index_region2(wage1,wage2) , sigma -1),(1 /  sigma)) - wage2;
 };
 
 //Modified form to derivations
 
 double mnew_wage_region1(double wage1, double wage2)
 {
-return new_wage_region1(wage1,wage2) + wage1;
+	return new_wage_region1(wage1,wage2) + wage1;
 };
 
 double mnew_wage_region2(double wage1, double wage2)
 { 
-return new_wage_region2(wage1,wage2) - wage2;
+	return new_wage_region2(wage1,wage2) - wage2;
 };
 
 //Modified form to derivations
@@ -106,12 +108,12 @@ return new_wage_region2(wage1,wage2) - wage2;
 
 double average_real_wage(double real_wage1, double real_wage2)
 {
-return 0;
+	return 0;
 };
 
 double new_lamda(double average_real_wage0, double real_wage1) //The value of the population share in the next
 {
-return 0;
+	return 0;
 
 };
 
@@ -187,15 +189,23 @@ double dw1, dw2, segedw1, segedw2;
 double jacobi[2][2], determinans, jacobiinverz[2][2];
 
 //fejléc
-printf("Iteráció \t w1 \t d1w1 \t d2w1 \t w2 \t d1w2 \t d2w2 \t g1 \t  g2 \t jöv1 \t jöv2 \t célfv. \n");
+if (extrainfo)
+{
 
+	printf("Iteráció \t w1 \t d1w1 \t d2w1 \t w2 \t d1w2 \t d2w2 \t g1 \t  g2 \t jöv1 \t jöv2 \t célfv. \n");
+
+};
 
 //fájl megnyitás
 
 FILE * pFile;
 pFile = fopen ( "neg.txt" , "w" );
-fprintf(pFile,"Iteráció \t w1 \t dw1 \t w2 \t dw2 \t g1 \t  g2 \t jöv1 \t jöv2 \t célfv. \n");
+if (extrainfo)
+{
+	
+	fprintf(pFile,"Iteráció \t w1 \t dw1 \t w2 \t dw2 \t g1 \t  g2 \t jöv1 \t jöv2 \t célfv. \n");
 
+};
 //Kezdeti értékek kiíratása a képernyőre és fájlba
 
 textwrite(pFile,0,extrainfo);
@@ -243,17 +253,42 @@ textwrite(pFile,i,extrainfo);
 }
 
 //Fájl bezárása
-fclose (pFile);
+fclose(pFile);
 
 };
 
 double solve_equation_system()
 {
-int i;
+	int i;
 
-for(i=0;i<101;i++)
-{
-solve_equation(false);
-}
+//fejléc
+
+	printf("Iteráció \t w1 \t w2 \t g1 \t  g2 \t jöv1 \t jöv2 \t célfv. \n");
+
+
+//fájl megnyitás
+
+
+	FILE * pFile;
+	pFile = fopen ( "neg_lambda.txt" , "w" );
+	fprintf(pFile,"Lambda \t w1 \t w2 \t g1 \t  g2 \t jöv1 \t jöv2 \t célfv. \n");
+
+
+	for(i=0;i<101;i++)
+	{
+		lambda = i/100;
+		solve_equation(false);
+
+		printf("%3d \t %.3f \t %.3f \t %.3f \t %.3f \t %.3f \t  %.3f \t %.3f \n",i,w1,w2,price_index_region1(w1,w2) ,price_index_region2(w1,w2), income_region1(w1),income_region2(w2), new_wage_region1(w1,w2) +  new_wage_region2(w1,w2));
+
+
+		fprintf(pFile,"%3d \t %e \t %e \t %e \t %e \t %e \t  %e \t %e \n",i ,w1,w2,price_index_region1(w1,w2) ,price_index_region2(w1,w2), income_region1(w1),income_region2(w2), new_wage_region1(w1,w2) +  new_wage_region2(w1,w2));
+
+	}
+
+	fclose(pFile);
 
 };
+
+
+
